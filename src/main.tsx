8dc82879b67252bc550ea39242388a8a10f62d73
Pranshu Verma <pranshuverma1601@gmail.com>
2024-04-07 17:28:25 +0530

    light/dark theme added

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import router from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./App.tsx";
+import { ThemeProvider } from "./components/ThemeProvider.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  </React.StrictMode>
+);
